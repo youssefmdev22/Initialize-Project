d15b0dd065eb87d3f372143f193ac97bbc99899e
youssefmdev22 <youssefmdev22@gmail.com>
2025-09-21 01:26:36 +0300

    Initialize Project

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -1,53 +1,5 @@
 abstract class AppIcons {
-  
-  static const String iconArrowDown = "assets/icons/arrow_down.png";
+  static const String _iconsPath = "assets/icons";
 
-  static const String iconBackIcon = "assets/icons/back_icon.png";
-
-  static const String iconCall = "assets/icons/call.png";
-
-  static const String iconCameraEditProfile = "assets/icons/camera_edit_profile.png";
-
-  static const String iconCancelled = "assets/icons/cancelled.png";
-
-  static const String iconCard = "assets/icons/card.png";
-
-  static const String iconCompleted = "assets/icons/completed.png";
-
-  static const String iconEgypt = "assets/icons/Egypt.png";
-
-  static const String iconHomeTab = "assets/icons/home_tab.png";
-
-  static const String iconInvalid = "assets/icons/invalid.png";
-
-  static const String iconLanguage = "assets/icons/language.png";
-
-  static const String iconLine = "assets/icons/line.png";
-
-  static const String iconLineGray = "assets/icons/line_gray.png";
-
-  static const String iconLineGreen = "assets/icons/line_green.png";
-
-  static const String iconLocation = "assets/icons/location.png";
-
-  static const String iconLogoutLarge = "assets/icons/logout_large.png";
-
-  static const String iconLogoutSmall = "assets/icons/logout_small.png";
-
-  static const String iconMessage = "assets/icons/message.png";
-
-  static const String iconNotification = "assets/icons/notification.png";
-
-  static const String iconOrdersTab = "assets/icons/orders_tab.png";
-
-  static const String iconPending = "assets/icons/pending.png";
-
-  static const String iconProfileTab = "assets/icons/profile_tab.png";
-
-  static const String iconUpload = "assets/icons/upload.png";
-
-  static const String iconWhatsapp = "assets/icons/whatsapp.png";
-
-  static const String iconX1 = "assets/icons/X1.png";
+  static const String iconArrowDown = "$_iconsPath/arrow_down.png";
 }
-
